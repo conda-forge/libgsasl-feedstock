972739e59227f8b280c3a925d5f3d7987636d397
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-21 11:11:06 -0800

    enable osx arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 ./configure --with-gssapi-impl=mit --enable-static=no --prefix=$PREFIX
-make
-make check
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+make check -j${CPU_COUNT}
+fi
 make install
